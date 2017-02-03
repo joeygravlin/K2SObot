1ad7c45786cf310f332780ece7514229e6f6d425
Joseph Gravlin <joeygravlin@gmail.com>
2017-02-03 00:52:19 -0500

    Modify runGame.bat to run MyBot.py against all *Bot.py

--- runGame.bat
+++ runGame.bat
@@ -1,1 +1,13 @@
-.\halite.exe -d "30 30" "python MyBot.py" "python RandomBot.py"
+rem Windows batch scripts feel like Bizzaro bash scripts.
+rem Strange, yet strangely familiar.
+
+SETLOCAL
+SET MY_BOT=MyBot.py
+SET PYTHON=python
+
+rem if hash python3 2>/dev/null; then
+rem     %%PYTHON=python3
+rem fi
+
+for %%SOME_BOT in (*Bot.py) do ^
+    .\halite.exe -d "30 30" "%%PYTHON %%MY_BOT" "%%PYTHON %%SOME_BOT"
